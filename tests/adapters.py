9311dffa03a8d0746b0597aea1fd41d8033f3225
Thomas Bao <thomas.bao@airbnb.com>
2026-07-02 18:15:41 -0700

    set up wiring to run tests

--- tests/adapters.py
+++ tests/adapters.py
@@ -10,6 +10,7 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
 from cs336_basics.tokenizer import Tokenizer
+from cs336_basics.train_bpe import train_bpe
 
 def run_linear(
     d_in: int,
@@ -590,4 +591,4 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    return train_bpe(input_path, vocab_size, special_tokens)
