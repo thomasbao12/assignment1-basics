fix gradient clipping

tests/adapters.py
@@ -690,7 +690,24 @@ def run_gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm:
 
     The gradients of the parameters (parameter.grad) should be modified in-place.
     """
-    raise NotImplementedError
+    sum_squared = 0
+    
+    for parameter in parameters:
+        if parameter.grad is None:
+            continue
+        grad = parameter.grad
+        sum_squared +=  grad.square().sum()
+
+    l2_norm = math.sqrt(sum_squared)
+
+        
+    if l2_norm > max_l2_norm:
+        factor = max_l2_norm / (l2_norm + 1e-6)
+        for parameter in parameters:
+            if parameter.grad is None:
+                continue 
+            parameter.grad *= factor
+
 
 
 def get_adamw_cls() -> Any: