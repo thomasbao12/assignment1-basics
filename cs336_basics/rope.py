1760bfa1e40523680433a658ea96cc8187fab0b0
Thomas Bao <thomas.bao@airbnb.com>
2026-08-12 18:23:32 -0700

    in rope, precompute rotation matrices

--- cs336_basics/rope.py
+++ cs336_basics/rope.py
@@ -6,6 +6,21 @@ from torch import Tensor
 
 class RoPE(torch.nn.Module):
 
+    def _init_rotation_matrices(self):
+        k = torch.arange(0, self.d_k // 2, device = self.device)
+        frequencies = 1 / self.theta ** (2 * k / self.d_k) # shape(pairs)
+        angles = torch.arange(self.max_seq_length, device = self.device)[..., None] * frequencies[None, :] # shape(seq, pairs)
+        cos = angles.cos() # shape(seq, pairs)
+        sin = angles.sin() # shape(seq, pairs)
+        rotation_matrices = torch.stack(
+            [
+                torch.stack([cos, -sin], dim = -1),
+                torch.stack([sin, cos], dim = -1)
+            ],
+            dim = -2
+        ) # shape(seq, pairs, rows, cols) rows = 2 cols = 2
+        self.register_buffer("rotation_matrices", rotation_matrices, persistent=False)
+        
     def __init__(
         self,
         theta: float,
@@ -18,26 +33,15 @@ class RoPE(torch.nn.Module):
         self.d_k = d_k
         self.max_seq_length = max_seq_length
         self.device = device
-        # TODO create a buffer of precomputed cos and sin tensors
+        self._init_rotation_matrices()
     
     def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
-        k = torch.arange(0, self.d_k // 2, device = self.device)
-        frequencies = 1 / self.theta ** (2 * k / self.d_k) # shape(pairs)
-        angles = token_positions[..., None] * frequencies[None, :] # shape(seq, pairs)
-        cos = angles.cos() # shape(seq, pairs)
-        sin = angles.sin() # shape(seq, pairs)
-        rotation_matrices = torch.stack(
-            [
-                torch.stack([cos, -sin], dim = -1),
-                torch.stack([sin, cos], dim = -1)
-            ],
-            dim = -2
-        ) # shape(seq, pairs, rows, cols) rows = 2 cols = 2
         x_pairs = einops.rearrange(
             x, 
             "... seq (pairs two) -> ... seq pairs two",
             two = 2
         ) 
+        rotation_matrices = self.rotation_matrices[token_positions]
         rotated_x_pairs = einops.einsum(
             x_pairs,
             rotation_matrices,
